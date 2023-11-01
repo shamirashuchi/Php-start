b9dde5e6173770e48e3e0460734d7590aaa2422a
shamirashuchi <shamirashuchi@gmail.com>
2023-11-01 09:03:42 +0600

    first commit

---
+++ Demo.php
@@ -0,0 +1,23 @@
+<?php
+//echo "Hello World";
+
+//function hello()
+//{
+//    echo "Hello World";
+//}
+//hello();
+
+
+//class Example
+//{
+//    public function hello()
+//    {
+//        echo"Hello happy World";
+//    }
+//}
+// class er object create
+//class er age new dile object create hoy
+//Example class er age new diye example object create hoise
+//$example = new Example();
+//call hello function through object
+//$example->hello();
